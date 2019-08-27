3d68e9a879e9a8e8efa116dd6a70c94bb3d82c04
veryjos <jdelgado002@gmail.com>
2019-08-26 21:53:00 -0400

    Refactor rendering to have a single generic write function

--- src/render.rs
+++ src/render.rs
@@ -21,16 +21,28 @@ pub fn render_time(time: f64) {
         .map(|glyph| (glyph[0].chars().count(), glyph.len()))
         .fold((0, 0), |acc, (width, height)| (acc.0 + width, max(acc.1, height)));
 
+    // little helper to write to the screen
+    let mut write = {
+        let mut stdout = stdout();
+
+        move |out: &dyn AsRef<[u8]>| {
+            stdout.write(out.as_ref()).expect("Failed to write to stdout");
+        }
+    };
+
     // clear the screen
-    let mut stdout = stdout();
-    stdout.write(b"\x1b[2;J");
+    write(&"\x1b[2;J");
 
     // initialize the cursor at the center position
-    let terminal_bounds = term_size::dimensions().unwrap();
-    let origin = (
-        (terminal_bounds.0 / 2) - (output_bounds.0 / 2),
-        (terminal_bounds.1 / 2) - (output_bounds.1 / 2),
-    );
+    let origin = {
+        let terminal_bounds = term_size::dimensions()
+            .expect("Failed to get terminal dimensions");
+
+        (
+            (terminal_bounds.0 / 2) - (output_bounds.0 / 2),
+            (terminal_bounds.1 / 2) - (output_bounds.1 / 2),
+        )
+    };
 
     // draw each glyph
     let mut cursor = origin.clone();
@@ -41,8 +53,8 @@ pub fn render_time(time: f64) {
         let mut width = 0;
         for line in glyph {
             // move to the beginning of the line
-            stdout.write(format!("\x1b[{};{}H", cursor.1, cursor.0).as_bytes());
-            stdout.write(line.as_bytes());
+            write(&format!("\x1b[{};{}H", cursor.1, cursor.0));
+            write(line);
 
             cursor.1 += 1;
             width = max(width, line.chars().count());
@@ -51,5 +63,5 @@ pub fn render_time(time: f64) {
         cursor.0 += width;
     }
 
-    stdout.flush();
+    write(&"\n");
 }
